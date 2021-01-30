fdcc30967f3ab68e434d8fc40885807a3aded371
Ilettc <callum_i@yahoo.com>
2021-01-30 16:00:00 +0000

    Finished Project

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,77 @@
+import { useState } from "react";
+import axios from "axios";
+
+const projectID = "cdea6801-2dbd-4d4e-a8fc-34c31542ee18";
+
+const Modal = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const authObject = {
+      "Project-ID": projectID,
+      "User-Name": username,
+      "User-Secret": password,
+    };
+
+    try {
+      await axios.get("https://api.chatengine.io/chats", {
+        headers: authObject,
+      });
+
+      localStorage.setItem("username", username);
+      localStorage.setItem("password", password);
+
+      window.location.reload();
+      setError("");
+    } catch (err) {
+      setError("Oops, incorrect credentials.");
+    }
+  };
+
+  return (
+    <div className="wrapper">
+      <div className="form">
+        <h1 className="title">Chat Application</h1>
+
+        <div
+          style={{ color: "white", textAlign: "center", marginBottom: "1rem" }}
+        >
+          <h4>Demo User</h4>
+          <p>Username: demo</p>
+          <p>Password: password123</p>
+        </div>
+
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            className="input"
+            placeholder="Username"
+            required
+          />
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="input"
+            placeholder="Password"
+            required
+          />
+          <div align="center">
+            <button type="submit" className="button">
+              <span>Start chatting</span>
+            </button>
+          </div>
+        </form>
+        <h1>{error}</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
